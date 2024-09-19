6902ac4cfcdc53031ba3bb1fc9793a6c36549ecc
N.Nakasato <nakasato@u-aizu.ac.jp>
2024-09-19 13:09:30 +0900

    fix exporting from HF

--- export.py
+++ export.py
@@ -536,7 +536,7 @@ def load_hf_model(model_path):
     config.dim = hf_model.config.hidden_size
     config.n_layers = hf_model.config.num_hidden_layers
     config.n_heads = hf_model.config.num_attention_heads
-    config.n_kv_heads = hf_model.config.num_attention_heads
+    config.n_kv_heads = hf_model.config.num_key_value_heads
     config.vocab_size = hf_model.config.vocab_size
     config.hidden_dim = hf_model.config.intermediate_size
     config.norm_eps = hf_model.config.rms_norm_eps
@@ -564,9 +564,19 @@ def load_hf_model(model_path):
         layer.attention.wq.weight = nn.Parameter(
             permute_reverse(hf_dict[f"model.layers.{i}.self_attn.q_proj.weight"])
         )
-        layer.attention.wk.weight = nn.Parameter(
-            permute_reverse(hf_dict[f"model.layers.{i}.self_attn.k_proj.weight"])
-        )
+        if config.n_heads == config.n_kv_heads:
+            layer.attention.wk.weight = nn.Parameter(
+                permute_reverse(hf_dict[f"model.layers.{i}.self_attn.k_proj.weight"])
+            )
+        else:
+            layer.attention.wk.weight = nn.Parameter(
+                permute_reverse(
+                    hf_dict[f"model.layers.{i}.self_attn.k_proj.weight"],
+                    config.n_kv_heads,
+                    (config.dim // config.n_heads * config.n_kv_heads),
+                    config.dim
+                )
+            )
         layer.attention.wv.weight = nn.Parameter(
             hf_dict[f"model.layers.{i}.self_attn.v_proj.weight"]
         )
