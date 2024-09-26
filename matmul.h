0f1d673d80bb36bc57b2b3f9846bc1c0bfe201fb
N.Nakasato <nakasato@u-aizu.ac.jp>
2024-09-26 09:33:41 +0900

    fix

---
+++ matmul.h
@@ -0,0 +1,11 @@
+{
+  int i;
+#pragma omp parallel for private(i)
+  for (i = 0; i < d; i++) {
+    float val = 0.0f;
+    for (int j = 0; j < n; j++) {
+      val += w[i * n + j] * x[j];
+    }
+    xout[i] = val;
+  }
+}
